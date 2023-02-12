f35c399712301c3424fcb8f48571e64da03e5372
binayapuri <97520897+binayapuri@users.noreply.github.com>
2023-02-12 23:06:24 +0545

    second

---
+++ ApexRestaurant.Repository/RepositoryModule.cs
@@ -0,0 +1,21 @@
+using Microsoft.Extensions.DependencyInjection;
+using ApexRestaurant.Repository.RCustomer;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace ApexRestaurant.Repository
+{
+    public static class RepositoryModule
+    {
+        public static void Register(IServiceCollection services, string connection)
+        {
+            services.AddDbContext<RestaurantContext>(options => options.UseSqlServer(
+                connection,
+                // @"Server=DESKTOP-13L30S5\SQLEXPRESS;Initial Catalog=ApexRestaurantDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False",
+                builder => builder.MigrationsAssembly(Assembly.GetExecutingAssembly().GetName().Name)
+            ));
+
+            services.AddTransient<ICustomerRepository, CustomerRepository>();
+        }
+    }
+}
